90296de0d8fe4634fac907fec44c3fdb6b332e11
Chrisclrk9 <chrisclrk9@gmail.com>
2020-06-02 09:21:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const url = "https://api.themoviedb.org/3/search/movie?api_key=a876e7500012d962d40cf6ba7bd19019&language=en-US&query=batman&page=1&include_adult=false"
 
-
+// testing github
 
 fetch(url).then((response) => {
   return response.json();
