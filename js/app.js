67bc076accd4f8a46ff6cf4780a2a5cf0833c6d9
Chrisclrk9 <chrisclrk9@gmail.com>
2020-06-02 09:19:42 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const url = "https://api.themoviedb.org/3/search/movie?api_key=a876e7500012d962d40cf6ba7bd19019&language=en-US&query=batman&page=1&include_adult=false"
+
+
+
+fetch(url).then((response) => {
+  return response.json();
+}).catch((err) => alert("I have no idea what's going on!!!"))
+.then((movies) => {
+  movies.results.map(movie => {
+    console.log(movie.title, movie.overview)
+  })
+})
